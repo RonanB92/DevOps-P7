4ff6ecae237c62313b462673dbfc07b28c1bb6e8
RonanB92 <78919292+RonanB92@users.noreply.github.com>
2021-05-30 15:47:52 +0100

    Create DebitCard.java
    Added Debit Card class

---
+++ src/main/java/revolut/DebitCard.java
@@ -0,0 +1,13 @@
+package revolut;
+
+public class DebitCard {
+    private double balance = 110.0;
+
+    public void setBankBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBankBalance() {
+        return this.balance;
+    }
+}
